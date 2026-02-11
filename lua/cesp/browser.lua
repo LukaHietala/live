fe936b06d9b763dcc23304cb8bb454a97c2ca3fe
Luka Hietala <luka.hietala08@gmail.com>
2026-02-11 18:20:28 +0200

    can't test, pray that new telescope browser works

--- lua/cesp/browser.lua
+++ lua/cesp/browser.lua
@@ -8,9 +8,41 @@ function M.list_remote_files()
 	})
 end
 
--- Basic file picker. TODO: Add telescope picker that will be shown instead if
--- Telescope has been installed
-function M.open_file_browser(files, on_select)
+local function open_with_telescope(files, on_select)
+	local pickers = require("telescope.pickers")
+	local finders = require("telescope.finders")
+	local conf = require("telescope.config").values
+	local themes = require("telescope.themes")
+	local actions = require("telescope.actions")
+	local action_state = require("telescope.actions.state")
+
+	local opts = themes.get_ivy({
+		prompt_title = "Remote files",
+	})
+
+	pickers
+		.new(opts, {
+			finder = finders.new_table({
+				results = files,
+			}),
+			sorter = conf.generic_sorter({}),
+			attach_mappings = function(prompt_bufnr)
+				actions.select_default:replace(function()
+					local selection = action_state.get_selected_entry()
+					actions.close(prompt_bufnr)
+					if selection then
+						-- selection[1] is the file path string
+						on_select(selection[1])
+					end
+				end)
+				return true
+			end,
+		})
+		:find()
+end
+
+-- Used when Telescope is not installed. Asks for input first to filter, then selects
+local function open_with_fallback(files, on_select)
 	vim.ui.input(
 		{ prompt = "Filter files (leave empty for all): " },
 		function(input)
@@ -33,8 +65,6 @@ function M.open_file_browser(files, on_select)
 				return
 			end
 
-			-- Show the filtered list using builtin vim.ui.select (sanoinkuvaamattoman asiallista)
-			-- If user has telescope-ui-select plugin or some other picker that takes this it will be used :D
 			vim.ui.select(filtered, {
 				prompt = "Select remote file:",
 				kind = "file",
@@ -45,6 +75,16 @@ function M.open_file_browser(files, on_select)
 			end)
 		end
 	)
+end
+
+function M.open_file_browser(files, on_select)
+	local has_telescope, _ = pcall(require, "telescope")
+
+	if has_telescope then
+		open_with_telescope(files, on_select)
+	else
+		open_with_fallback(files, on_select)
+	end
 end
 
 function M.open_remote_file(path, content, on_complete)
